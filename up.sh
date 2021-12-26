12f4c0d065a1cf4a16a91f5c1e3e1b50734eae47
Doridian <git@doridian.net>
2021-12-26 12:18:13 -0800

    override

--- up.sh
+++ up.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -e
 
+cd "$(dirname "$0")"
+
 docker-compose -p spaceage pull
-docker-compose -p spaceage up -d --build --remove-orphans
+
+LOCALF="docker-compose.$(hostname).yml"
+if [ -f "$LOCALF" ]
+then
+    docker-compose -p spaceage -f docker-compose.yml -f "$LOCALF" up -d --build --remove-orphans
+else
+    docker-compose -p spaceage up -d --build --remove-orphans
+fi
