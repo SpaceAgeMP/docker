affced035a8a9def8ac794bc2b70d66bed0c8095
Doridian <git@doridian.net>
2024-06-18 21:21:45 -0700

    new compose

--- up.sh
+++ up.sh
@@ -3,12 +3,12 @@ set -e
 cd "$(dirname "$0")"
 git pull
 
-docker-compose -p spaceage pull "$@"
+docker compose -p spaceage pull "$@"
 
 LOCALF="docker-compose.$(hostname).yml"
 if [ -f "$LOCALF" ]
 then
-    docker-compose -p spaceage -f docker-compose.yml -f "$LOCALF" up -d --build --remove-orphans "$@"
+    docker compose -p spaceage -f docker-compose.yml -f "$LOCALF" up -d --build --remove-orphans "$@"
 else
-    docker-compose -p spaceage up -d --build --remove-orphans "$@"
+    docker compose -p spaceage up -d --build --remove-orphans "$@"
 fi
