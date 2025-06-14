b82761c86eb8185adaf457aa85a5bd5f35614c2a
Doridian <git@doridian.net>
2025-06-13 23:53:14 -0700

    Separate build

--- up.sh
+++ up.sh
@@ -3,14 +3,15 @@ set -e
 cd "$(dirname "$0")"
 git pull
 
-#export COMPOSE_BAKE=true
-
-docker compose -p spaceage pull "$@"
-
 LOCALF="docker-compose.$(hostname).yml"
-if [ -f "$LOCALF" ]
-then
-    docker compose -p spaceage -f docker-compose.yml -f "$LOCALF" up -d --build --remove-orphans "$@"
-else
-    docker compose -p spaceage up -d --build --remove-orphans "$@"
+if [ ! -f "$LOCALF" ]; then
+    LOCALF=/dev/null
 fi
+
+run_compose() {
+    docker compose -p spaceage -f docker-compose.yml -f "$LOCALF" "$@"
+}
+
+run_compose pull "$@"
+run_compose build "$@"
+run_compose up -d --remove-orphans "$@"
